a1a71ebcbb282a4d616c62d9a03adcf27c16b889
Karthik R <karram@gmail.com>
2013-06-29 13:40:12 -0400

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    //response.send('Hello World 2!');
+    var bfr = fs.readFileSync("index.html");
+    var contents = bfr.toString();
+    //console.log(contents);
+    response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
